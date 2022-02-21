1b041550901e7ddc8bb46d506b094e9c03dd3580
Diego Blanco <diegoblancotaboas@gmail.com>
2022-02-21 12:21:13 +0100

    Constructor

--- 01.php
+++ 01.php
@@ -1,4 +1,6 @@
-<?php include 'includes/header.php';
+<?php declare( strict_types = 1);
+
+include 'includes/header.php';
 
 // Definir una clase
 class Producto {
@@ -7,21 +9,22 @@ class Producto {
     public $precio;
     public $disponible;
 
+    //Constructor
+    public function __construct( string $nombre, int $precio, bool $disponible)
+    {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponible = $disponible;
+    }
 }
 
-$producto = new Producto();
-$producto->nombre = 'Tablet';
-$producto->precio = 200;
-$producto->disponible = true;
+$producto = new Producto('Tablet', 200, true);
 
 echo "<pre>";
 var_dump($producto);
 echo "</pre>";
 
-$producto2 = new Producto();
-$producto2->nombre = 'Monitor Curvo';
-$producto2->precio = 300;
-$producto2->disponible = true;
+$producto2 = new Producto('Monitor', 300, true);
 
 echo "<pre>";
 var_dump($producto2);
