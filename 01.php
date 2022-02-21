ad4b22deb8b3c473df8ffe1a19b4bfe8148ab04f
Diego Blanco <diegoblancotaboas@gmail.com>
2022-02-21 12:12:35 +0100

    Creacion de clases

--- 01.php
+++ 01.php
@@ -10,9 +10,21 @@ class Producto {
 }
 
 $producto = new Producto();
+$producto->nombre = 'Tablet';
+$producto->precio = 200;
+$producto->disponible = true;
 
 echo "<pre>";
 var_dump($producto);
+echo "</pre>";
+
+$producto2 = new Producto();
+$producto2->nombre = 'Monitor Curvo';
+$producto2->precio = 300;
+$producto2->disponible = true;
+
+echo "<pre>";
+var_dump($producto2);
 echo "</pre>";
 
 include 'includes/footer.php';
