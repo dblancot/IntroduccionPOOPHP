8582abf31c5a08c694551b92f8746bcf03fdd064
Diego Blanco <diegoblancotaboas@gmail.com>
2022-02-28 19:08:31 +0100

    Autoload de clases

--- 08.php
+++ 08.php
@@ -1,4 +1,16 @@
 <?php include 'includes/header.php';
 
+// Incluir las otras clases
+
+
+function mi_autoload($clase) {    
+    require __DIR__ . '/class/' . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+$clientes = new Clientes();
+
 
 include 'includes/footer.php';
