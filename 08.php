61ee840a10c2622a6d71904aae63091601e8de8d
Diego Blanco <diegoblancotaboas@gmail.com>
2022-03-01 20:53:44 +0100

    Namespaces

--- 08.php
+++ 08.php
@@ -1,16 +1,18 @@
 <?php include 'includes/header.php';
 
+use App\Clientes;
+use App\Detalles;
+
 // Incluir las otras clases
-
-
-function mi_autoload($clase) {    
-    require __DIR__ . '/class/' . $clase . '.php';
+function mi_autoload($clase) {  
+    $partes = explode('\\', $clase);  
+    require __DIR__ . '/class/' . $partes[1] . '.php';
 }
 
 spl_autoload_register('mi_autoload');
 
+
 $detalles = new Detalles();
 $clientes = new Clientes();
-
 
 include 'includes/footer.php';
